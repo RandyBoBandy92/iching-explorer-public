7fef9fe521f0e3a8744e5dc4b05b1ff2c1d2e968
Randy Gulak <randy@randygulak.com>
2024-10-09 22:53:09 -0700

    Update public files after build

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "iching-explorer-v1";
+const BASE_NAME = "/iching-explorer-public";
+const urlsToCache = [
+  BASE_NAME + "/",
+  BASE_NAME + "/index.html",
+  BASE_NAME + "/manifest.json",
+  ...Array(64)
+    .keys()
+    .map((i) => `${BASE_NAME}/hexagramJSONS/hexagram${i + 1}.json`),
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
+
+// Clean up old caches
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
